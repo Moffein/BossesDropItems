d888d89bd25e582e721eae69262db4cfa27e0c7d
Moffein <22742925+Moffein@users.noreply.github.com>
2022-12-06 13:15:39 -0800

    Changed default settings and ArtifactOfPotential compat.

--- BossesDropItems/BossesDropItems.cs
+++ BossesDropItems/BossesDropItems.cs
@@ -3,7 +3,9 @@ using BepInEx.Configuration;
 using RoR2;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using UnityEngine.Networking;
 
 namespace R2API.Utils
@@ -16,7 +18,8 @@ namespace R2API.Utils
 
 namespace BossesDropItems
 {
-    [BepInPlugin("com.Moffein.BossesDropItems", "Bosses Drop Items", "1.2.3")]
+    [BepInDependency("zombieseatflesh7.ArtifactOfPotential", BepInDependency.DependencyFlags.SoftDependency)]
+    [BepInPlugin("com.Moffein.BossesDropItems", "Bosses Drop Items", "1.3.0")]
     public class BossesDropItems : BaseUnityPlugin
     {
         public static float blankChance = 0f;
@@ -35,20 +38,32 @@ namespace BossesDropItems
         public static bool sacrificeOnly = false;
         public static int overloadingWormBonus = 4;
 
+        public static bool artifactPotentialLoaded = false;
+
+        private static BodyIndex LunarGolemBodyIndex;
+        private static BodyIndex LunarWispBodyIndex;
+        private static BodyIndex LunarExploderBodyIndex;
         private static BodyIndex ElectricWormBodyIndex;
 
         private static GameModeIndex SimulacrumIndex;
 
+        public static PickupDropTable tier1Drops = Addressables.LoadAssetAsync<PickupDropTable>("RoR2/Base/Common/dtTier1Item.asset").WaitForCompletion();
+        public static PickupDropTable tier2Drops = Addressables.LoadAssetAsync<PickupDropTable>("RoR2/Base/Common/dtTier2Item.asset").WaitForCompletion();
+        public static PickupDropTable tier3Drops = Addressables.LoadAssetAsync<PickupDropTable>("RoR2/Base/Common/dtTier3Item.asset").WaitForCompletion();
+        public static GameObject potentialPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/DLC1/OptionPickup/OptionPickup.prefab").WaitForCompletion();
+
         public void Awake()
         {
+            artifactPotentialLoaded = BepInEx.Bootstrap.Chainloader.PluginInfos.ContainsKey("zombieseatflesh7.ArtifactOfPotential");
+
             sacrificeOnly = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Sacrifice Only"), false, new ConfigDescription("Only drop items if the Artifact of Sacrifice is enabled.")).Value;
             enableTeleDrops = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Enable Teleporter Drops"), true, new ConfigDescription("Teleporters drop items for all players when all bosses are killed.")).Value;
-            teleBossDropsItems = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Teleporter Bosses Drop Items"), true, new ConfigDescription("Allows Teleporter Bosses to drop items when killed.")).Value;
-            hordeDropsItems = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Horde of Many Drops Items"), true, new ConfigDescription("Allows Horde of Many Bosses to drop items when killed.")).Value;
+            teleBossDropsItems = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Teleporter Bosses Drop Items"), false, new ConfigDescription("Allows Teleporter Bosses to drop items when killed.")).Value;
+            hordeDropsItems = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Horde of Many Drops Items"), false, new ConfigDescription("Allows Horde of Many Bosses to drop items when killed.")).Value;
             guaranteeTeleGreen = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Guaranteed Green from Tele Bosses"), false, new ConfigDescription("Guarantees that Teleporter Bosses will drop only Green-tier items or better. Does not apply to Horde of Many.")).Value;
-            maxExtraDrops = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Max Extra Drops"), 7, new ConfigDescription("Maximum amount of extra items Elite Bosses can drop. Regular Elite Bosses will attempt to drop 3 extra items, while Tier 2 Elite Bosses will attempt to drop 17.")).Value;
-            minEliteBossDrops = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Min Extra Elite Boss Drops"), 1, new ConfigDescription("Minimum amount of extra items Elite Bosses can drop.")).Value;
-            overloadingWormBonus = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Overloading Worm Extra Boss Drops"), 4, new ConfigDescription("Extra item drops from Overloading Worms.")).Value;
+            maxExtraDrops = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Max Extra Drops"), 0, new ConfigDescription("Maximum amount of extra items Elite Bosses can drop. Regular Elite Bosses will attempt to drop 3 extra items, while Tier 2 Elite Bosses will attempt to drop 17.")).Value;
+            minEliteBossDrops = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Min Extra Elite Boss Drops"), 0, new ConfigDescription("Minimum amount of extra items Elite Bosses can drop.")).Value;
+            overloadingWormBonus = base.Config.Bind<int>(new ConfigDefinition("Drop Settings", "Overloading Worm Extra Boss Drops"), 0, new ConfigDescription("Extra item drops from Overloading Worms.")).Value;
             lunarChimerasDropItems = base.Config.Bind<bool>(new ConfigDefinition("Drop Settings", "Lunar Chimeras Drop Items"), false, new ConfigDescription("Makes Lunar Chimeras able to drop items when they show up as bosses during Mitchell Phase 2.")).Value;
 
             blankChance = base.Config.Bind<float>(new ConfigDefinition("Item Tier Settings", "Blank Chance"), 0f, new ConfigDescription("Chance for bosses to drop no items.")).Value;
@@ -61,7 +76,10 @@ namespace BossesDropItems
             On.RoR2.BodyCatalog.Init += (orig) =>
             {
                  orig();
-                 ElectricWormBodyIndex = BodyCatalog.FindBodyIndex("ElectricWormBody");
+                LunarGolemBodyIndex = BodyCatalog.FindBodyIndex("LunarGolemBody");
+                LunarExploderBodyIndex = BodyCatalog.FindBodyIndex("LunarExploderBody");
+                LunarWispBodyIndex = BodyCatalog.FindBodyIndex("LunarWispBody");
+                ElectricWormBodyIndex = BodyCatalog.FindBodyIndex("ElectricWormBody");
             };
 
             On.RoR2.GameModeCatalog.LoadGameModes += (orig) =>
@@ -93,7 +111,12 @@ namespace BossesDropItems
                 
                 if (NetworkServer.active && sacrificeCheck && aliveBeforeHit && !self.alive)
                 {
-                    if (self.gameObject && self.body && (self.body.isChampion || (!self.body.isChampion && bossEnemy && hordeDropsItems && Run.instance.gameModeIndex != SimulacrumIndex)) && self.body.teamComponent && self.body.teamComponent.teamIndex == TeamIndex.Monster && (!bossEnemy || teleBossDropsItems) && (lunarChimerasDropItems || !(self.body.baseNameToken == "LUNARWISP_BODY_NAME" || self.body.baseNameToken == "LUNARGOLEM_BODY_NAME" || self.body.baseNameToken == "LUNAREXPLODER_BODY_NAME")))
+                    if (self.gameObject && self.body &&
+                        (self.body.isChampion
+                            || (!self.body.isChampion && bossEnemy && hordeDropsItems && Run.instance.gameModeIndex != SimulacrumIndex))
+                        && self.body.teamComponent && self.body.teamComponent.teamIndex == TeamIndex.Monster
+                        && (!bossEnemy || teleBossDropsItems)
+                        && (lunarChimerasDropItems || !(self.body.bodyIndex == LunarWispBodyIndex || self.body.bodyIndex == LunarGolemBodyIndex || self.body.bodyIndex == LunarExploderBodyIndex)))
                     {
                         DeathRewards reward = self.gameObject.GetComponent<DeathRewards>();
                         PickupIndex bossPickupIndex = reward ? PickupCatalog.FindPickupIndex(reward.bossPickup.pickupName) : PickupIndex.none;
@@ -131,6 +154,8 @@ namespace BossesDropItems
 
         private static void DropItem(CharacterBody victimBody, PickupIndex bossPickup, bool greenMinimum, float randomOffset)
         {
+            bool isYellow = false;
+            ItemTier tier = ItemTier.NoTier;
             List<PickupIndex> list;
             float total = whiteChance + greenChance + redChance + yellowChance + blankChance;
             if (greenMinimum || !Util.CheckRoll(100f * blankChance / total, 0))
@@ -139,6 +164,7 @@ namespace BossesDropItems
                 if (!greenMinimum && Util.CheckRoll(100f * whiteChance / total, 0))//drop white
                 {
                     list = Run.instance.availableTier1DropList;
+                    tier = ItemTier.Tier1;
                 }
                 else
                 {
@@ -146,6 +172,7 @@ namespace BossesDropItems
                     if (Util.CheckRoll(100f * greenChance / total, 0))//drop green
                     {
                         list = Run.instance.availableTier2DropList;
+                        tier = ItemTier.Tier2;
                     }
                     else
                     {
@@ -153,6 +180,7 @@ namespace BossesDropItems
                         if (Util.CheckRoll(100f * redChance / total, 0))//drop red
                         {
                             list = Run.instance.availableTier3DropList;
+                            tier = ItemTier.Tier3;
                         }
                         else
                         {
@@ -162,17 +190,85 @@ namespace BossesDropItems
                                 {
                                     bossPickup
                                 };
+                                isYellow = true;
                             }
                             else//drop green if no boss pickup available
                             {
                                 list = Run.instance.availableTier2DropList;
+                                tier = ItemTier.Tier2;
                             }
                         }
                     }
                 }
+
+
+
                 int index = Run.instance.treasureRng.RangeInt(0, list.Count);
-                PickupDropletController.CreatePickupDroplet(list[index], victimBody.transform.position, new Vector3(UnityEngine.Random.Range(0f, randomOffset), 20f, UnityEngine.Random.Range(0f, randomOffset)));
+                PickupIndex originalIndex = list[index];
+
+                if (isYellow || !BossesDropItems.IsPotentialArtifactActive())
+                {
+                    PickupDropletController.CreatePickupDroplet(originalIndex, victimBody.transform.position, new Vector3(UnityEngine.Random.Range(0f, randomOffset), 20f, UnityEngine.Random.Range(0f, randomOffset)));
+                }
+                else
+                {
+                    PickupDropTable pdt;
+
+                    switch (tier)
+                    {
+                        case ItemTier.Tier3:
+                            pdt = BossesDropItems.tier3Drops;
+                            break;
+                        case ItemTier.Tier2:
+                            pdt = BossesDropItems.tier2Drops;
+                            break;
+                        default:
+                            pdt = BossesDropItems.tier1Drops;
+                            break;
+                    }
+
+                    PickupPickerController.Option[] options = PickupPickerController.GenerateOptionsFromDropTable(3, pdt, Run.instance.treasureRng);
+                    if (options.Length > 0)
+                    {
+                        bool alreadyHasPickup = false;
+                        foreach (PickupPickerController.Option o in options)
+                        {
+                            if (o.pickupIndex == originalIndex)
+                            {
+                                alreadyHasPickup = true;
+                                break;
+                            }
+                        }
+                        if (!alreadyHasPickup) options[0].pickupIndex = originalIndex;
+
+                        PickupDropletController.CreatePickupDroplet(new GenericPickupController.CreatePickupInfo
+                        {
+                            pickupIndex = PickupCatalog.FindPickupIndex(tier),
+                            pickerOptions = options,
+                            rotation = Quaternion.identity,
+                            prefabOverride = BossesDropItems.potentialPrefab
+                        }, victimBody.transform.position, Vector3.up * 20f);
+                    }
+                    else
+                    {
+                        PickupDropletController.CreatePickupDroplet(originalIndex, victimBody.transform.position, new Vector3(UnityEngine.Random.Range(0f, randomOffset), 20f, UnityEngine.Random.Range(0f, randomOffset)));
+                    }
+                }
             }
+        }
+
+
+        public static bool IsPotentialArtifactActive()
+        {
+            bool isActive = false;
+            if (artifactPotentialLoaded) isActive = IsPotentialArtifactActiveInternal();
+            return isActive;
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining | MethodImplOptions.NoOptimization)]
+        private static bool IsPotentialArtifactActiveInternal()
+        {
+            return RunArtifactManager.instance && RunArtifactManager.instance.IsArtifactEnabled(ArtifactOfPotential.PotentialArtifact.Potential);
         }
     }
 }
